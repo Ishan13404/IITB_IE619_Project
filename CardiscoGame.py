0a79c96bad3eec3ac831b1cfd7dddaba0285d073
Ishan <ishangrover13404@gmail.com>
2023-10-30 04:06:09 +0530

    Added help section and other features in Cardisco

--- CardiscoGame.py
+++ CardiscoGame.py
@@ -33,15 +33,14 @@ font = pygame.font.SysFont(None, 40)
 def TextOnScreen(text, colour, x, y):
     screen_text = font.render(text, True, colour)
     gameWindow.blit(screen_text, [x, y])
-
-def Draw_Game_Board(Gameboard, Available_options, Player_Pointer, Player_to_Move):
+def Draw_Game_Board(Gameboard, Available_options, Player_Pointer, Player_to_Move, Last_Picked_Card):
     TextOnScreen("*** CARDISCO ***", (128,204,199), Screen_Width*0.4, Screen_Height*0.1-50)
-    TextOnScreen("Player to Move: Player "+ str(Player_to_Move)+"                                                           Press e to exit.", black, Screen_Width*0.1, Screen_Height*0.1)
-    TextOnScreen("Available options are highlighted in        ." + "                              Press r to restart", black, Screen_Width*0.1, Screen_Height*0.1+50)
+    TextOnScreen("Player to Move: Player "+ str(Player_to_Move)+"            Last Picked Card: "+str(Last_Picked_Card)+"              Press e to Exit.", black, Screen_Width*0.1, Screen_Height*0.1)
+    TextOnScreen("Available options are highlighted in        ." + "                              Press r to Restart", black, Screen_Width*0.1, Screen_Height*0.1+50)
     TextOnScreen("RED", red, Screen_Width*0.1+490, Screen_Height*0.1+50)
-    TextOnScreen("Currently selected card is highlighted in          .", black, Screen_Width*0.1, Screen_Height*0.1+100)
+    TextOnScreen("Currently selected card is highlighted in          ."+"                    Press h for Help.", black, Screen_Width*0.1, Screen_Height*0.1+100)
     TextOnScreen("BLUE", blue, Screen_Width*0.1+550, Screen_Height*0.1+100)
-    TextOnScreen("Use the LEFT-RIGHT Arrow keys to navigate. Press ENTER to make a move.", black, Screen_Width*0.1, Screen_Height*0.1+150)    
+    TextOnScreen("Use the LEFT-RIGHT Arrow keys to navigate. Press SPACEBAR to make a move.", black, Screen_Width*0.1, Screen_Height*0.1+150)    
     pygame.draw.rect(gameWindow, black, [10, Screen_Height/2, Screen_Width-20, Card_Height])
     pygame.draw.rect(gameWindow, white, [15 , (Screen_Height/2)+5, Screen_Width-30, Card_Height*0.95])
     for i in range(12):
@@ -58,8 +57,8 @@ def Display_Result(Winning_Player):
     while(True):
         gameWindow.fill(white)
         TextOnScreen("Player "+str(Winning_Player)+" Wins!!", green, Screen_Width*0.4, Screen_Height*0.4)
-        TextOnScreen("Press e to exit", green, Screen_Width*0.4, Screen_Height*0.4+50)
-        TextOnScreen("Press r to restart", green, Screen_Width*0.4, Screen_Height*0.4+100)
+        TextOnScreen("Press e to Exit", green, Screen_Width*0.4, Screen_Height*0.4+50)
+        TextOnScreen("Press r to Restart", green, Screen_Width*0.4, Screen_Height*0.4+100)
         pygame.display.update()
         clock.tick(FPS)
         for event in pygame.event.get():
@@ -79,6 +78,45 @@ def Homescreen():
             if event.type == pygame.KEYDOWN :
                 return
 
+def Display_Rules():
+
+    rules_text = [
+        "Rules to play the Game :",
+        "1. Gameplay:",
+        "   Two players take alternating turns selecting cards.",
+        "   Each turn, a player must choose one card based on the card pickup rules.",
+        "2. Card Pickup Rules:",
+        "   The Card Numbered 7 is initially set as the Last Picked Card.",
+        "   In subsequent turns, players can only pick cards that meet these conditions :-",
+        "     a. The card is adjacent to the last picked card.",
+        "     b. The card's number is double the number of the last picked card.",
+        "     c. The card's number is half the number of last picked card (except for odd-numbered cards).",
+        "3. Objective:",
+        "   Players aim to strategically select cards to create a position that prevents their opponent",
+        "     from making any legal moves.",
+        "4. Winning Condition:",
+        "   The player wins when their opponent cannot pick up any more cards.",
+        "   In simpler terms, the player who places their opponent in a situation where they can't"
+        ,"      make a move emerges victorious.",
+        "",
+        "Press any key to jump back into the Game! All the Best!"
+    ]
+
+    # Font for displaying text
+    font = pygame.font.Font(None, 36)
+
+    # Create a list of text surfaces for each line of the rules
+    rule_text_surfaces = [font.render(line, True, black) for line in rules_text]
+
+    while(True):
+        gameWindow.fill(white)
+        for i, text_surface in enumerate(rule_text_surfaces):
+            gameWindow.blit(text_surface, (50, 30 + i * 30))
+        pygame.display.update()
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN or event.type == pygame.QUIT:
+                return
 
 def CardiscoGame():
     n = 13
@@ -112,7 +150,7 @@ def CardiscoGame():
         # print("\n")
         Player_Choice=7
         gameWindow.fill(white)
-        Draw_Game_Board(Gameboard, Available_options, Player_Pointer, Player_to_Move)
+        Draw_Game_Board(Gameboard, Available_options, Player_Pointer, Player_to_Move,Last_Picked_Card)
         pygame.display.update()
         clock.tick(FPS)
         for event in pygame.event.get():
@@ -121,20 +159,22 @@ def CardiscoGame():
             if event.type == pygame.KEYDOWN and event.key == pygame.K_r:
                 CardiscoGame()
                 Game_Over=True
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_h:
+                Display_Rules()
             # Handling Events
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT and Player_Pointer in list(range(12)):
                     Player_Pointer+=1
                 if event.key == pygame.K_LEFT and Player_Pointer in list(range(1,13)):
                     Player_Pointer-=1
-                if event.key==pygame.K_RETURN and Player_Pointer+1 in Available_options:
+                if event.key==pygame.K_SPACE and Player_Pointer+1 in Available_options:
                     Player_Choice=Player_Pointer+1
                     Last_Picked_Card = Player_Choice
                     Player_to_Move = 3-Player_to_Move
                     Gameboard[Player_Choice-1] = "X"
     ###################################################
             gameWindow.fill(white)
-            Draw_Game_Board(Gameboard, Available_options, Player_Pointer, Player_to_Move)
+            Draw_Game_Board(Gameboard, Available_options, Player_Pointer, Player_to_Move, Last_Picked_Card)
             pygame.display.update()
             clock.tick(FPS)
 
